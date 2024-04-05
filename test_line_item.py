24e55e0044c75f2a5e683f153acc62cb83c5a014
Richard Stokes <richardstokes77@gmail.com>
2024-04-05 16:46:43 -0400

    Add tests for LineItem

---
+++ test_line_item.py
@@ -0,0 +1,18 @@
+from list_item import ListItem
+import unittest
+
+class TestLineItem(unittest.TestCase):
+
+    def test_init(self):
+        foobar = ListItem()
+        self.assertEqual(foobar.title, "")
+        self.assertFalse(foobar.complete)
+    
+    def test_toggle_complete(self):
+        foobar = ListItem("Foo my bar")
+        foobar.toggle_complete()
+        self.assertTrue(foobar.complete)
+
+
+if __name__ == '__main__':
+    unittest.main()
